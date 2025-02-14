86e332ce2a5c08193d6902c2167cb07daea2daa3
Kiss Ádám <113520135+KissAdambat@users.noreply.github.com>
2025-02-14 11:06:47 +0100

    Add files via upload
    sql fix

--- sneaker-heaven.sql
+++ sneaker-heaven.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 12. 19:10
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.2.12
+-- Létrehozás ideje: 2025. Feb 14. 11:06
+-- Kiszolgáló verziója: 10.4.20-MariaDB
+-- PHP verzió: 7.3.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `sneaker-heaven`
+-- Adatbázis: `sneakerheaven`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,41 @@ CREATE TABLE `user` (
   `UserName` varchar(100) NOT NULL,
   `Email` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `user`
+--
+
+INSERT INTO `user` (`ID`, `UserName`, `Email`, `Password`) VALUES
+(1, 'pcheater0', 'rpescott0@nationalgeographic.com', 'gG68,g0o*'),
+(2, 'bwellstead1', 'wmcgiffin1@cnn.com', 'cK0c#m&!4ZJ3?w5'),
+(3, 'ksansun2', 'smeekin2@fastcompany.com', 'yJ3,zEss'),
+(4, 'jdisney3', 'cpencott3@ibm.com', 'xA6V@3H|hZ)0C_c0'),
+(5, 'csimchenko4', 'gstolberg4@wufoo.com', 'iN6V}G+h'),
+(6, 'sshermar5', 'htawse5@i2i.jp', 'uI3@PIV3H&!a'),
+(7, 'omilne6', 'jotter6@skype.com', 'hS7&T4~`YI/j_a_'),
+(8, 'icasaroli7', 'mhayers7@amazon.co.jp', 'nR13OeXXzJm@hP&'),
+(9, 'hbilton8', 'sbrayley8@artisteer.com', 'rV0jE5ELn'),
+(10, 'pcheater0', 'rpescott0@nationalgeographic.com', 'gG68,g0o*'),
+(11, 'bwellstead1', 'wmcgiffin1@cnn.com', 'cK0c#m&!4ZJ3?w5'),
+(12, 'ksansun2', 'smeekin2@fastcompany.com', 'yJ3,zEss'),
+(13, 'jdisney3', 'cpencott3@ibm.com', 'xA6V@3H|hZ)0C_c0'),
+(14, 'csimchenko4', 'gstolberg4@wufoo.com', 'iN6VG+h'),
+(15, 'sshermar5', 'htawse5@i2i.jp', 'uI3@PIV3H&!a'),
+(16, 'omilne6', 'jotter6@skype.com', 'hS7&T4YI/j_a_'),
+(17, 'icasaroli7', 'mhayers7@amazon.co.jp', 'nR13OeXXzJm@hP&'),
+(18, 'hbilton8', 'sbrayley8@artisteer.com', 'rV0jE5ELn'),
+(19, 'pcheater0', 'rpescott0@nationalgeographic.com', 'gG68,g0o*'),
+(20, 'bwellstead1', 'wmcgiffin1@cnn.com', 'cK0c#m&!4ZJ3?w5'),
+(21, 'ksansun2', 'smeekin2@fastcompany.com', 'yJ3,zEss'),
+(22, 'jdisney3', 'cpencott3@ibm.com', 'xA6V@3H|hZ)0C_c0'),
+(23, 'csimchenko4', 'gstolberg4@wufoo.com', 'iN6VG+h'),
+(24, 'sshermar5', 'htawse5@i2i.jp', 'uI3@PIV3H&!a'),
+(25, 'omilne6', 'jotter6@skype.com', 'hS7&T4YI/j_a_'),
+(26, 'icasaroli7', 'mhayers7@amazon.co.jp', 'nR13OeXXzJm@hP&'),
+(27, 'hbilton8', 'sbrayley8@artisteer.com', 'rV0jE5ELn'),
+(28, 'tsugarman9', 'kdavidai9@go.com', 'aS5qMPWzJYRGW');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -52,7 +86,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT a táblához `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
