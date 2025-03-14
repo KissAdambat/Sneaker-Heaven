185a3c9fd95231040caecd876deb307ab5103ec6
MarkusBercel <markusb@kkszki.hu>
2025-03-14 12:17:40 +0100

    the first shoe is done

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,544 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace sneaker_heaven
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int accid = 0;
+        internal static Connect Conn = new Connect();
+        Dictionary<string, int> kosarcart = new Dictionary<string, int>() { };
+        List<string> af1 = new List<string>() { "Triple White", "Triple Black" };
+        List<int> af1meret = new List<int>() { 37, 40, 42, 425 , 43, 46 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GridLog.Visibility = Visibility.Hidden;
+            GridReg.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            GridFej1.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            Uj1.Visibility = Visibility.Hidden;
+            Uj2.Visibility = Visibility.Hidden;
+            Pass1.Visibility = Visibility.Hidden;
+            Pass2.Visibility = Visibility.Hidden;
+            Updatepassword.Visibility = Visibility.Hidden;
+            Gridkosar.Visibility = Visibility.Hidden;
+            GridCip1.Visibility = Visibility.Hidden;
+            szin.ItemsSource = af1;
+            meret.ItemsSource = af1meret;
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e) ///login button
+        {
+            GridLog.Visibility = Visibility.Visible;
+            GridMain.Visibility = Visibility.Hidden;
+            GridReg.Visibility = Visibility.Hidden;
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e) ///registerbutton
+        {
+            string username = (string)textbox1.Text;
+            string jelszo = (string)textbox2.Text;
+
+            if (username == "" || jelszo == "")
+            {
+                MessageBox.Show("No data found");
+            }
+            else
+            {
+                Conn.Connection.Open();
+
+                string sql = "SELECT `ID`, `UserName`, `Password` FROM `user` WHERE 1";
+
+                MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                bool regisztralt = false;
+
+                dr.Read();
+
+                do
+                {
+                    var felhasznalo = new
+                    {
+                        UserNameAdatbazis = dr.GetString(1),
+                        JelszoAdatbazis = dr.GetString(2),
+                    };
+
+                    if (username == felhasznalo.UserNameAdatbazis && jelszo == felhasznalo.JelszoAdatbazis)
+                    {
+                        accid = dr.GetInt32(0);
+                        regisztralt = true;
+                    }
+
+
+                }
+                while (dr.Read());
+
+                if (regisztralt == true)
+                {
+
+                    MessageBox.Show("Successful login");
+                    GridLog.Visibility = Visibility.Hidden;
+                    GridMain.Visibility = Visibility.Visible;
+                    Grid2fej.Visibility = Visibility.Hidden;
+                    GridFej1.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("No user found");
+                }
+
+                dr.Close();
+
+                Conn.Connection.Close();
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = (string)textbox3.Text;
+            string email = (string)textbox4.Text;
+            string jelszo = (string)textbox5.Text;
+            string jelszo2 = (string)textbox6.Text;
+
+            if (username == "" || email == "" || jelszo == "" || !kukac())
+            {
+                MessageBox.Show("No data found");
+            }
+            else
+            {
+                if (jelszo != jelszo2)
+                {
+                    MessageBox.Show("The two passwords don't match");
+                }
+                else
+                {
+                    try
+                    {
+
+                        Conn.Connection.Open();
+
+                        string sql = $"INSERT INTO `user`(`UserName`, `Email`, `Password`) VALUES ('{username}','{email}','{jelszo}')";
+
+                        MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+                        cmd.ExecuteNonQuery();
+
+                        Conn.Connection.Close();
+
+                        MessageBox.Show("Successful register");
+
+                        GridReg.Visibility = Visibility.Hidden;
+                        GridMain.Visibility = Visibility.Hidden;
+                        Grid2fej.Visibility = Visibility.Visible;
+                        GridFej1.Visibility = Visibility.Hidden;
+                        GridLog.Visibility = Visibility.Visible;
+
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("The database is not connected");
+                    }
+                }
+            }
+        }
+
+
+        private bool kukac()
+        {
+            string email = (string)textbox4.Text;
+
+
+            foreach (char c in email)
+            {
+                if (c == '@')
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e) ///regisztráció button
+        {
+            GridReg.Visibility = Visibility.Visible;
+            GridMain.Visibility = Visibility.Hidden;
+            GridLog.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) //home button
+        {
+            GridReg.Visibility = Visibility.Hidden;
+            GridMain.Visibility = Visibility.Visible;
+            GridLog.Visibility = Visibility.Hidden;
+        }
+
+        private void logo_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Visible;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            GridCip1.Visibility = Visibility.Hidden;
+            Conn.Connection.Open();
+
+            string sql = $"SELECT `UserName`, `Email`, `Password` FROM `user` WHERE ID = '{accid}'; ";
+
+            MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+
+            dr.Read();
+
+            do
+            {
+                var felhasznalo = new
+                {
+                    Username = dr.GetString(0),
+                    Emaild = dr.GetString(1),
+                };
+
+                accname.Content = $"{felhasznalo.Username}";
+                accemail.Content = $"{felhasznalo.Emaild}";
+            }
+            while (dr.Read());
+
+
+            dr.Close();
+            Conn.Connection.Close();
+
+            passlock.Visibility = Visibility.Hidden;
+        }
+
+        private void passunlock_Click(object sender, RoutedEventArgs e)
+        {
+            Conn.Connection.Open();
+
+            string sql = $"SELECT `Password` FROM `user` WHERE ID = '{accid}'; ";
+
+            MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+
+            dr.Read();
+
+            do
+            {
+                var felhasznalo = new
+                {
+                    Userpass = dr.GetString(0),
+                };
+
+                accpassword.Content = $"{felhasznalo.Userpass}";
+            }
+            while (dr.Read());
+
+
+            dr.Close();
+            Conn.Connection.Close();
+
+            passunlock.Visibility = Visibility.Hidden;
+            passlock.Visibility = Visibility.Visible;
+        }
+
+        private void passlock_Click(object sender, RoutedEventArgs e)
+        {
+            accpassword.Content = "********";
+            passlock.Visibility = Visibility.Hidden;
+            passunlock.Visibility = Visibility.Visible;
+        }
+
+        private void Men_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Visible;
+        }
+
+        private void Women_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Visible;
+        }
+
+        private void Kids_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Visible;
+        }
+
+        private void Discount_Click(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Visible;
+        }
+
+        private void Newpass_Click(object sender, RoutedEventArgs e)
+        {
+            Uj1.Visibility = Visibility.Visible;
+            Uj2.Visibility = Visibility.Visible;
+            Pass1.Visibility = Visibility.Visible;
+            Pass2.Visibility = Visibility.Visible;
+            Updatepassword.Visibility = Visibility.Visible;
+        }
+
+        private void Updatepassword_Click(object sender, RoutedEventArgs e)
+        {
+
+            string jelszo1 = Pass1.Text;
+            string jelszo2 = Pass2.Text;
+
+            if (jelszo1 != jelszo2)
+            {
+                MessageBox.Show("The two passwords don't match");
+            }
+            else
+            {
+                Conn.Connection.Open();
+                string sql = $"UPDATE `user` SET `Password`='{jelszo1}' WHERE `ID` = '{accid}';";
+
+                MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+                cmd.ExecuteNonQuery();
+
+                Conn.Connection.Close();
+            }
+        }
+
+        private void Newpass_Click_1(object sender, RoutedEventArgs e)
+        {
+            Uj1.Visibility = Visibility.Visible;
+            Uj2.Visibility = Visibility.Visible;
+            Pass1.Visibility = Visibility.Visible;
+            Pass2.Visibility = Visibility.Visible;
+            Updatepassword.Visibility = Visibility.Visible;
+        }
+
+        private void Updatepassword_Click_1(object sender, RoutedEventArgs e)
+        {
+            string jelszo1 = Pass1.Text;
+            string jelszo2 = Pass2.Text;
+
+            if (jelszo1 != jelszo2)
+            {
+                MessageBox.Show("The two passwords don't match");
+            }
+            else
+            {
+                Conn.Connection.Open();
+                string sql = $"UPDATE `user` SET `Password`='{jelszo1}' WHERE `ID` = '{accid}';";
+
+                MySqlCommand cmd = new MySqlCommand(sql, Conn.Connection);
+                cmd.ExecuteNonQuery();
+
+                Conn.Connection.Close();
+
+                MessageBox.Show("Successful password update!");
+            }
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            GridMain.Visibility = Visibility.Visible;
+            GridAccaunt.Visibility = Visibility.Hidden;
+        }
+
+        private void Men_Click_1(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Visible;
+        }
+
+        private void Women_Click_1(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Visible;
+        }
+
+        private void Kids_Click_1(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Visible;
+        }
+
+        private void Discount_Click_1(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Visible;
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            GridMain.Visibility = Visibility.Visible;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            GridLog.Visibility = Visibility.Hidden;
+            GridReg.Visibility = Visibility.Hidden;
+            GridFej1.Visibility = Visibility.Hidden;
+            Grid2fej.Visibility = Visibility.Visible;
+            MessageBox.Show("Logged out successfully!");
+        }
+
+        private void cart_Click(object sender, RoutedEventArgs e)
+        {
+            Gridferfi.Visibility = Visibility.Hidden;
+            Gridgyerek.Visibility = Visibility.Hidden;
+            Gridno.Visibility = Visibility.Hidden;
+            Gridakcios.Visibility = Visibility.Hidden;
+            GridMain.Visibility = Visibility.Hidden;
+            GridAccaunt.Visibility = Visibility.Hidden;
+            GridLog.Visibility = Visibility.Hidden;
+            GridReg.Visibility = Visibility.Hidden;
+            GridFej1.Visibility = Visibility.Visible;
+            GridCip1.Visibility = Visibility.Hidden;
+            kosarlist.ItemsSource = GetKosarcart().Keys;
+            Gridkosar.Visibility = Visibility.Visible;
+        }
+
+        private Dictionary<string, int> GetKosarcart()
+        {
+            return kosarcart;
+        }
+
+        public int fizetendo = 0;
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            GridMain.Visibility = Visibility.Hidden;
+            GridCip1.Visibility = Visibility.Visible;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void meret_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+            string szin2 = Convert.ToString(szin.SelectedItem);
+            int meret2 = Convert.ToInt32(meret.SelectedItem);
+            if (szin2 == af1[0])
+            {
+                if (meret2 == af1meret[0] || meret2 == af1meret[1] || meret2 == af1meret[2] || meret2 == af1meret[4])
+                {
+                    if (new1.IsChecked == true)
+                    {
+                        ar.Content = "Price: 80€";
+                        fizetendo = fizetendo + 80;
+                        kosarcart.Add("Air Force 1 Low '07", 80);
+                    }
+                    else
+                    {
+                        ar.Content = "Price: 60€";
+                        fizetendo = fizetendo + 60;
+                        kosarcart.Add("Air Force 1 Low '07", 60);
+                    }
+                }
+                else
+                {
+                    ar.Content = "We dont have that size. Sorry";
+                }
+                
+            }
+            else
+            {
+                if (meret2 == af1meret[3] || meret2 == af1meret[5])
+                {
+                    if (new1.IsChecked == true)
+                    {
+                        ar.Content = "Price : 80€ ";
+                        fizetendo = fizetendo + 80;
+                        kosarcart.Add("Air Force 1 Low '07", 80);
+                    }
+                    else
+                    {
+                        ar.Content = "Price: 60€" ;
+                        fizetendo = fizetendo + 60;
+                        kosarcart.Add("Air Force 1 Low '07", 60);
+                    }
+                }
+                else
+                {
+                    ar.Content = "We dont have that size. Sorry";
+                }
+            }
+        }
+    }
+}
---
+++ sneaker-heaven.sql
@@ -0,0 +1,90 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP DATABASE IF EXISTS sneakerheaven;
+CREATE DATABASE IF NOT EXISTS sneakerheaven 
+CHARACTER SET utf8 
+COLLATE utf8_hungarian_ci;
+
+USE sneakerheaven;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `sneakerheaven`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `user`
+--
+
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- A tábla adatainak kiíratása `user`
+--
+
+INSERT INTO `user` (`ID`, `UserName`, `Email`, `Password`) VALUES
+(1, 'pcheater0', 'rpescott0@nationalgeographic.com', 'gG68,g0o*'),
+(2, 'bwellstead1', 'wmcgiffin1@cnn.com', 'cK0c#m&!4ZJ3?w5'),
+(3, 'ksansun2', 'smeekin2@fastcompany.com', 'yJ3,zEss'),
+(4, 'jdisney3', 'cpencott3@ibm.com', 'xA6V@3H|hZ)0C_c0'),
+(5, 'csimchenko4', 'gstolberg4@wufoo.com', 'iN6V}G+h'),
+(6, 'sshermar5', 'htawse5@i2i.jp', 'uI3@PIV3H&!a'),
+(7, 'omilne6', 'jotter6@skype.com', 'hS7&T4~`YI/j_a_'),
+(8, 'icasaroli7', 'mhayers7@amazon.co.jp', 'nR13OeXXzJm@hP&'),
+(9, 'hbilton8', 'sbrayley8@artisteer.com', 'rV0jE5ELn'),
+(10, 'pcheater0', 'rpescott0@nationalgeographic.com', 'gG68,g0o*'),
+(11, 'bwellstead1', 'wmcgiffin1@cnn.com', 'cK0c#m&!4ZJ3?w5'),
+(12, 'ksansun2', 'smeekin2@fastcompany.com', 'yJ3,zEss'),
+(13, 'jdisney3', 'cpencott3@ibm.com', 'xA6V@3H|hZ)0C_c0'),
+(14, 'csimchenko4', 'gstolberg4@wufoo.com', 'iN6VG+h'),
+(15, 'sshermar5', 'htawse5@i2i.jp', 'uI3@PIV3H&!a'),
+(16, 'omilne6', 'jotter6@skype.com', 'hS7&T4YI/j_a_'),
+(17, 'icasaroli7', 'mhayers7@amazon.co.jp', 'nR13OeXXzJm@hP&'),
+(18, 'hbilton8', 'sbrayley8@artisteer.com', 'rV0jE5ELn'),
+(19, 'pcheater0', 'rpescott0@nationalgeographic.com', 'gG68,g0o*'),
+(20, 'bwellstead1', 'wmcgiffin1@cnn.com', 'cK0c#m&!4ZJ3?w5'),
+(21, 'ksansun2', 'smeekin2@fastcompany.com', 'yJ3,zEss'),
+(22, 'jdisney3', 'cpencott3@ibm.com', 'xA6V@3H|hZ)0C_c0'),
+(23, 'csimchenko4', 'gstolberg4@wufoo.com', 'iN6VG+h'),
+(24, 'sshermar5', 'htawse5@i2i.jp', 'uI3@PIV3H&!a'),
+(25, 'omilne6', 'jotter6@skype.com', 'hS7&T4YI/j_a_'),
+(26, 'icasaroli7', 'mhayers7@amazon.co.jp', 'nR13OeXXzJm@hP&'),
+(27, 'hbilton8', 'sbrayley8@artisteer.com', 'rV0jE5ELn'),
+(28, 'tsugarman9', 'kdavidai9@go.com', 'aS5qMPWzJYRGW');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
